8941062220a26d00246405b852d54c260a4315d1
Christian Marcellino <cammar12177@gmail.com>
2025-09-04 01:28:19 -0500

    display the parameter names in the error

--- Lisp/Diagnostics/Reports/WrongArgumentCountReportMessage.cs
+++ Lisp/Diagnostics/Reports/WrongArgumentCountReportMessage.cs
@@ -5,6 +5,6 @@ namespace Lisp.Exceptions;
 public class WrongArgumentCountReportMessage : ReportMessage
 {
     public WrongArgumentCountReportMessage(List<IdentifierNode> expectedArguments, int passedCount, int? requiredCount = null)
-        : base($"{requiredCount ?? expectedArguments.Count} arguments were required ({string.Join(", ", expectedArguments)}), but {passedCount} were passed.")
+        : base($"{requiredCount ?? expectedArguments.Count} arguments were required ({string.Join(", ", expectedArguments.Select(x => x.Text))}), but {passedCount} was passed.")
     { }
 }
